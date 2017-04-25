81a3bc43321c229e3a04cedec907ba8eed4e8b0a
florian_aschenbrenner <a0000805@unet.univie.ac.at>
2017-04-25 18:26:55 +0200

    newOfferScreen added

---
+++ app/src/main/java/com/example/florian/p2p_lender/DetailedOffer.java
@@ -0,0 +1,13 @@
+package com.example.florian.p2p_lender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_offer);
+    }
+}
---
+++ app/src/main/java/com/example/florian/p2p_lender/MyLoans.java
@@ -0,0 +1,13 @@
+package com.example.florian.p2p_lender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyLoans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_loans);
+    }
+}
