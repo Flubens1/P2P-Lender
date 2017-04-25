b1fb3c19fc92a363903c3f072e3623e760df47b7
florian_aschenbrenner <a0000805@unet.univie.ac.at>
2017-04-25 15:33:09 +0200

    newOfferScreen added

--- app/src/main/java/com/example/florian/p2p_lender/MyPocket.java
+++ app/src/main/java/com/example/florian/p2p_lender/MyPocket.java
@@ -1,13 +1,32 @@
 package com.example.florian.p2p_lender;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MyPocket extends AppCompatActivity {
+public class MyPocket extends AppCompatActivity implements View.OnClickListener{
+
+    Button newOffer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_pocket);
+
+        newOffer = (Button)findViewById(R.id.offerButton);
+        newOffer.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==newOffer) {
+            Intent intent = new Intent(this, NewOffer.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/florian/p2p_lender/NewOffer.java
@@ -0,0 +1,13 @@
+package com.example.florian.p2p_lender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_offer);
+    }
+}
