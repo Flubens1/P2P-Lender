f4e72d791b74ad9b8de1f24d0bbf09c32769d446
florian_aschenbrenner <a0000805@unet.univie.ac.at>
2017-04-25 15:04:43 +0200

    zweiter commit

--- app/src/main/java/com/example/florian/p2p_lender/MainActivity.java
+++ app/src/main/java/com/example/florian/p2p_lender/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.florian.p2p_lender;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button logIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logIn = (Button)findViewById(R.id.logInButton);
+        logIn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v==logIn) {
+            Intent intent = new Intent(this, MyPocket.class);
+            startActivity(intent);
+        }
+
     }
 }
